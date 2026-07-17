cb596c12899d0d60fc906f20bf08455ff4b11ff8
kingron10 <kiruiaron783@gmail.com>
2026-07-17 23:12:57 +0300

    Add basic styling for body and button elements
    Signed-off-by: kingron10 <kiruiaron783@gmail.com>

---
+++ script.js
@@ -0,0 +1,15 @@
+body{
+background:#111;
+color:white;
+font-family:Arial;
+text-align:center;
+padding:20px;
+}
+
+button{
+padding:15px;
+font-size:18px;
+background:#ff444f;
+color:white;
+border:none;
+border-radius:8px;
